e245253c01b33e969358d98f1bb179b41112df65
Messier <810152974@qq.com>
2023-05-06 23:48:55 +0800

    chore： add api options note

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -3,8 +3,10 @@ export const TIME_OUT = 10000
 
 let BASE_URL = ''
 if (import.meta.env.MODE === 'production') {
+	// BASE_URL = 'http://152.136.185.210:5000'
 	BASE_URL = 'http://codercba.com:5000'
 } else {
+	// BASE_URL = 'http://152.136.185.210:4000'
 	BASE_URL = 'http://codercba.com:5000'
 }
 
